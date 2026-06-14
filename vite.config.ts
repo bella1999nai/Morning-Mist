a602dc3302fa2d6417d702cf7d7a87a348f50441
bella1999nai <81353297+bella1999nai@users.noreply.github.com>
2026-06-14 10:49:52 +0800

    Set base path for Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    Base:'/Morning-Mist/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
